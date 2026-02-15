a344c20ac1e23e21d7a52b7822cfe28ba1b32414
benbilal94 <benhambilal94@gmail.com>
2026-02-15 13:43:12 +0100

    module 0 completed

--- m0/hard1/index.js
+++ m0/hard1/index.js
@@ -1,3 +1,4 @@
+"use strict";
 // H1: Light/Dark Toggle
 // Maak een knop die de pagina wisselt tussen een licht en donker thema.
 
@@ -7,4 +8,33 @@
 // Verander minstens 3 stijleigenschappen (bijvoorbeeld achtergrondkleur, tekstkleur en linkkleur)
 // Laat de knop ook van tekst veranderen ("Donker thema" / "Licht thema")
 
-let knop = document.getElementById
+let knop = document.getElementById("themaKnop");
+let bodyAchterGrondList = document.getElementsByTagName("Body");
+let linkTag = document.getElementById("linkTag");
+
+let isDarkMode = false;
+
+knop.addEventListener("click", function () {
+
+  if (isDarkMode === false) {
+
+    for (let i = 0; i < bodyAchterGrondList.length; i++) {
+      bodyAchterGrondList[i].style.backgroundColor = "black";
+      bodyAchterGrondList[i].style.color = "white";
+    }
+    linkTag.style.color = "white";
+
+    isDarkMode = true
+
+  }else {
+
+    for (let i = 0; i < bodyAchterGrondList.length; i++) {
+      bodyAchterGrondList[i].style.backgroundColor = "";
+      bodyAchterGrondList[i].style.color = "";
+    }
+    linkTag.style.color = "";
+
+    isDarkMode = false
+
+  }
+});
